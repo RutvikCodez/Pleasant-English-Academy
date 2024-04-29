b145f24e1cb68a4cf6c9e25e30bb2c040be29360
RutvikCodez <rutvikdarji21@gnu.ac.in>
2024-04-29 07:55:47 +0530

    buid error solved

--- components/gallery/Gallery.tsx
+++ components/gallery/Gallery.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import TitleDesc from "../TitleDesc";
-import { imagesData, videoData } from "@/utils/getData";
+import { imagesData } from "@/utils/getData";
 
 const Gallery = () => {
   return (
